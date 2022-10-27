7cb6337ff9a9c3f815ce72d4ed9f926220be1288
Nicholas Camargo <113930660+ncamargo22@users.noreply.github.com>
2022-10-27 17:52:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# from methods_help import method_help
+# from functions import function , welcome, add_numbers, num_divdedFrom_add_numbers
+from returnStuff import power
+
+# #method_help()
+# welcome("Yahaira")
+# added = add_numbers(45,5)
+# print(added)
+
+# final = num_divdedFrom_add_number(45, added)
+# print(final)
+# function()
+powerof = power(2,9)
+print(powerof)
