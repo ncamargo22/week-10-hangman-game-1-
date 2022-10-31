7cac6382471245b0536de5299559e528abe6cd14
Nicholas Camargo <113930660+ncamargo22@users.noreply.github.com>
2022-10-31 17:25:00 +0000

    3 digit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from methods_help import method_help
 # from functions import function , welcome, add_numbers, num_divdedFrom_add_numbers
-from returnStuff import power
+# from returnStuff import power
+from dynamic_functions import check_3Digits
 
 # #method_help()
 # welcome("Yahaira")
@@ -10,5 +11,8 @@ from returnStuff import power
 # final = num_divdedFrom_add_number(45, added)
 # print(final)
 # function()
-powerof = power(2,9)
-print(powerof)
+# powerof = power(2,9)
+# print(powerof)
+# sum = 526 + 345
+result = check_3Digits([555, 999, 122, 6000])
+print(result)
